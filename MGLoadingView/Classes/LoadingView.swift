293da14b4d8bdb183d3f25084f85ee1677b4ecd6
Mustafa GUNES <gunes149@gmail.com>
2019-02-20 09:46:39 +0300

    project created

---
+++ MGLoadingView/Classes/LoadingView.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingView.swift
+//  MGLoadingView
+//
+//  Created by Mustafa GUNES on 20.02.2019.
+//  Copyright © 2019 Mustafa GUNES. All rights reserved.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+    
+}

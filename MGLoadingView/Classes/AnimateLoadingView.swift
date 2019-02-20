ad10e9ff0efd6a3701cbb6b7396646e766f47ca4
Mustafa GUNES <gunes149@gmail.com>
2019-02-20 09:52:43 +0300

    LoadingView UI created and code written

---
+++ MGLoadingView/Classes/AnimateLoadingView.swift
@@ -0,0 +1,13 @@
+//
+//  AnimateLoadingView.swift
+//  MGLoadingView
+//
+//  Created by Mustafa GUNES on 20.02.2019.
+//  Copyright © 2019 Mustafa GUNES. All rights reserved.
+//
+
+import UIKit
+
+class AnimateLoadingView: UIView {
+
+}
